f2a4a5510f106502254c39af0c9801e2984b5257
Christian Alcalde <cc.alcalde@yahoo.com>
2022-03-22 16:06:29 -0700

    completed serial.py

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,17 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start=100):
+        self.start = start
+        self.default = start
+
+    def generate(self):
+        self.start += 1
+        return self.start - 1
+
+    def reset(self):
+        self.start = self.default
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start} default={self.default}>"
