e60c130caa4ad9bab84c0a622dd4a6a5775ab08b
davidjeffers <ohsjeffers@gmail.com>
2022-03-22 19:20:03 -0500

    wordfinder and words.txt

--- serial.py
+++ serial.py
@@ -19,15 +19,19 @@ class SerialGenerator:
     """
 
     def __init__(self, start=100):
+        """Create serial with start varable and set default to the start"""
         self.start = start
         self.default = start
 
     def generate(self):
+        """Adds one to start and returns start minus one"""
         self.start += 1
         return self.start - 1
 
     def reset(self):
+        """Sets start back to default value"""
         self.start = self.default
 
     def __repr__(self):
+        """Documenting Instance"""
         return f"<SerialGenerator start={self.start} default={self.default}>"
--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,35 @@
+from random import choice
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, path):
+        """Creates WordFinder from path"""
+
+        file = open(path)
+        self.word_list = self.find_words(file)
+        print(f"{len(self.word_list)} words read")
+
+    def __repr__(self):
+        return f"<WordFinder words_list = {self.word_list}>"
+
+    def find_words(self, file):
+        """Reads file given a path strips off line breaks"""
+
+        return [line.strip() for line in file]
+
+    def random(self):
+        """Chooses random word from word list"""
+
+        return choice(self.word_list)
+
+
+class SpecialWordFinder(WordFinder):
+    """SpecialWordFinder: finds only words omits comments and new lines"""
+
+    def find_words(self, file):
+        """Finds words that are not comments and not new lines, sets words_list"""
+
+        self.word_list = [word for word in super().find_words(file) if not word.startswith("#") and word != '']
+        return self.word_list
+
+
